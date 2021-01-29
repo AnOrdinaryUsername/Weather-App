175e330f622e0c2a8a56e18e54e1a5d5dd447cad
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-01-28 22:32:55 -0800

    refactor: Add visual error message on fail

--- src/index.js
+++ src/index.js
@@ -1,55 +1,49 @@
+import ErrorAlert from './components/ErrorAlert/ErrorAlert';
 import './styles.css';
-
-// bat cave is a valid city name
-// More silly names here: https://en.wikipedia.org/wiki/Wikipedia:Unusual_place_names
-const grabWeatherData = async (searchQuery) => {
-  let weatherData = '';
-
-  if (typeof searchQuery === 'object') {
-    const { latitude, longitude } = { ...searchQuery };
-    weatherData = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=9d958a66e735735b56e66b55bba5ada5`
-    );
-  } else {
-    weatherData = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=9d958a66e735735b56e66b55bba5ada5`
-    );
-  }
-
-  const result = await weatherData.json();
-  document.querySelector('.result').textContent = result.name;
-  return result;
-};
+import WeatherData from './WeatherData';
 
 const findCurrentLocationWeather = () => {
-  const userLocationButton = document.querySelector('#geolocation');
+    const userLocationButton = document.querySelector('#geolocation');
 
-  const useLocation = (position) => {
-    const { coords } = position;
+    const useLocation = (position) => {
+        const { coords } = position;
 
-    const userLocation = {
-      latitude: coords.latitude,
-      longitude: coords.longitude,
+        const userLocation = {
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+        };
+
+        const forecast = new WeatherData(userLocation);
+        forecast.showWeather();
     };
 
-    grabWeatherData(userLocation);
-  };
+    const handleLocationError = (browserHasGeolocation) => {
+        const errorMessage = browserHasGeolocation
+            ? 'The Geolocation service failed. Enable it by clicking the marker symbol in the address bar.'
+            : 'Your browser does not support geolocation.';
 
-  const handleLocationError = (browserHasGeolocation) => {
-    document.querySelector('#result').textContent = browserHasGeolocation
-      ? 'Error: The Geolocation service failed'
-      : 'Error: Your browser does not support eolocation.';
-  };
+        ErrorAlert.message(errorMessage);
+        ErrorAlert.toggleView();
+    };
 
-  userLocationButton.addEventListener('click', () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(useLocation, () => {
-        handleLocationError(true);
-      });
-    } else {
-      handleLocationError(false);
-    }
-  });
+    userLocationButton.addEventListener('click', () => {
+        if (ErrorAlert.isPresent) {
+            ErrorAlert.toggleView();
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(useLocation, () => {
+                handleLocationError(true);
+            });
+        } else {
+            handleLocationError(false);
+        }
+    });
 };
+
+// For hot reloading in webpack-dev-server.
+if (module.hot) {
+    module.hot.accept();
+}
 
 findCurrentLocationWeather();
