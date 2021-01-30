39cba6cc539ce71a5261b892a465eeb174c64966
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-01-29 20:08:05 -0800

    feat: Add search box functionality

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,29 @@
+import WeatherData from '../../apis/WeatherData';
+import './SearchBox.css';
+
+const findSearchLocationWeather = () => {
+    const searchBox = document.querySelector('#search-city');
+    const searchBoxButton = document.querySelector('.search__button');
+
+    const displayWeather = () => {
+        const requestedLocation = searchBox.value;
+        const forecast = new WeatherData();
+        forecast.showWeather(requestedLocation);
+    };
+
+    searchBoxButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        displayWeather();
+    });
+
+    searchBox.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            event.stopPropagation();
+            displayWeather();
+        }
+    });
+};
+
+export default findSearchLocationWeather;
