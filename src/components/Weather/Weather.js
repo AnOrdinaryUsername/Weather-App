8dfc93943d4619b891f6e80e2288be33e8c43b02
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-01-31 10:17:02 -0800

    fix: Change order of elements in weather display
    On gh-pages, the text nodes weren't being targeted correctly and instead
    left nodes being empty. Add 'nbsp;' to <h2> to target the text node and
    remove nested spans since they're inline elements.

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -5,13 +5,13 @@ const selectElement = (query) => document.querySelector(query);
 const Weather = {
     editDisplay: ({ image, location, phrase, misc }) => {
         const weatherImage = selectElement('.weather__image');
-        weatherImage.src = `http://openweathermap.org/img/wn/${image.name}@4x.png`;
+        weatherImage.src = `https://openweathermap.org/img/wn/${image.name}@4x.png`;
         weatherImage.alt = image.alt;
 
         selectElement('.weather__city').firstChild.textContent = `${location.city}`;
         selectElement('.weather__country').textContent = `, ${location.country}`;
 
-        selectElement('.weather__temp').firstChild.textContent = `${phrase.temp}°F `;
+        selectElement('.weather__temp').textContent = `${phrase.temp}°F `;
         const properCase = phrase.desc[0].toUpperCase() + phrase.desc.slice(1);
         selectElement('.weather__phrase').textContent = `/ ${properCase}`;
     },
