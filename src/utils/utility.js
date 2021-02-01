f6d4cb55531b73f8b8f7359e856186a55b35eb42
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-01 12:50:02 -0800

    feat: Store utility functions in utils folder

---
+++ src/utils/utility.js
@@ -0,0 +1,10 @@
+// Convert first letter in string to uppercase
+const convertToProperCase = (string) => string[0].toUpperCase() + string.slice(1);
+
+const Conversion = {
+    convertToMetric: () => {
+        console.log('test');
+    },
+};
+
+export { convertToProperCase, Conversion };
