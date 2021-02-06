eb0c6ee2901887b09917556f2a8f8db076c19191
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-05 16:27:40 -0800

    feat: Check local storage for color theme
    Default theme is dark, but implementation checks for preferred color theme and
    local storage to see which theme to use.

--- src/root/App.js
+++ src/root/App.js
@@ -1,11 +1,45 @@
 import '@fortawesome/fontawesome-free/js/all';
 import WeatherData from '../apis/WeatherData';
+import changeColorTheme from '../components/ColorThemeButton';
 import findCurrentLocationWeather from '../components/Geolocation';
 import findSearchQueryWeather from '../components/SearchBox';
 import switchMeasurementSystem from '../components/SystemButton';
 
 const pageInit = () => {
+    const changeToSunIcon = () => {
+        const theme = document.querySelector('button.theme');
+        const moon = document.querySelector('.fa-moon');
+        const sun = document.querySelector('.fa-sun');
+
+        // Removes dark class in button.
+        theme.classList.toggle('dark');
+
+        moon.classList.add('remove-svg');
+        sun.classList.remove('remove-svg');
+    };
+
+    const detectColorTheme = () => {
+        // Initial color theme.
+        let theme = 'dark';
+        const storedTheme = localStorage.getItem('theme');
+
+        if (storedTheme) {
+            if (storedTheme === 'light') {
+                theme = 'light';
+            }
+        } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+            theme = 'light';
+        }
+
+        if (theme === 'light') {
+            changeToSunIcon();
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+    };
+
     window.addEventListener('DOMContentLoaded', () => {
+        detectColorTheme();
+
         const forecast = new WeatherData();
         forecast.showWeather('Bat Cave');
     });
@@ -13,6 +47,7 @@ const pageInit = () => {
 
 const App = () => {
     pageInit();
+    changeColorTheme();
     findCurrentLocationWeather();
     findSearchQueryWeather();
     switchMeasurementSystem();
